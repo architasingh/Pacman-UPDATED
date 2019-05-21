f8a064bd3c205fcc515dcbee8defc25203284b73
AyanaYanai <50027587+AyanaYanai@users.noreply.github.com>
2019-05-21 11:10:15 -0700

    Update coins.java

--- coins.java
+++ coins.java
@@ -1,4 +1,24 @@
+import java.awt.Image;
+import java.awt.Rectangle;
 
-public class coins {
-
+public class Coin extends GameObject{
+	private Image coin;
+	private boolean show;
+	private int locx, locy;
+	private Rectangle rect;
+	
+	
+	Coin(int x, int y, int w, int h){
+		super(x,y,w,h, "coin.png");
+		locx = x;
+		locy = y;
+		coin = getImage("coin.png");
+		show = true;
+		
+	}
+	
+	public void disappear() {
+		show = false;
+		coin = null;
+	}
 }
