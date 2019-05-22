cc7f150263d6e7a5d10b313d9bd934414843caae
AyanaYanai <50027587+AyanaYanai@users.noreply.github.com>
2019-05-22 11:05:36 -0700

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -19,33 +19,7 @@ public class Wall  extends GameObject{
 		locy = y;
 		
 	}
-	public static double area(Rectangle rect) {
-		return rect.width*rect.height;
-	}
-	
-	public boolean hit(GameObject go) {
-		Rectangle over = collisionRect(go);
-		if(over.isEmpty())
-			return false;
-		double thisArea = area(rect), 
-				goArea = area(go.getRect()),
-				overArea = area(over);
-		return overArea > Math.min(thisArea, goArea)*HIT_THRESHOLD;
-	}
-	public Rectangle collisionRect(GameObject go) {
-		return this.rect.intersection(go.getRect());
-	}
 
-	public boolean mostlyOverlapping(GameObject go) {
-		Rectangle over = collisionRect(go);
-		double thisArea = area(this.rect), 
-				goArea = area(go.getRect()),
-				overArea = area(over);
-		return overArea > Math.min(thisArea, goArea)*OVERLAP_THRESHOLD;
-	}
-	public Rectangle getRect() {
-		return this.rect;
-	}
 	public void draw(Graphics g) {
 		g.setColor(Color.PINK);
 		g.fillRect(locx, locy, width, height);
